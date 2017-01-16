ffd7d354d1b8f9d561763a04ba1c6d7c281c1bbd
Sara Han <marsa926@gmail.com>
2017-01-16 23:21:41 +0000

    edited my download_avatar

--- project1/github-avatar-downloader1/dowload_avatars.js
+++ project1/github-avatar-downloader1/dowload_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var GITHUB_USER = "marsa926";
+var GITHUB_TOKEN = "01dc079d65f0c0355b38bb4388e04ca227341553";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
