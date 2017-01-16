e53abb58eed15f18fd759328ba6e7f005a05feb4
Sara Han <marsa926@gmail.com>
2017-01-16 21:05:21 +0000

    added module, runner files

---
+++ assignment1/http-functions-opener.js
@@ -0,0 +1,15 @@
+
+
+var getHTML = require('./http-functions.js');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+function printHTML (html) {
+    console.log(html);
+}
+
+getHTML(requestOptions,printHTML);
+
---
+++ assignment1/http-functions.js
@@ -0,0 +1,22 @@
+
+var https = require('https');
+
+module.exports = function getHTML(options, callback) {
+https.get(options,function(response){
+  var buf = "";
+    response.setEncoding('utf8');
+
+    response.on('data', function (data){
+      buf += data.toString();
+      console.log('Chunk Received. Length:', data.length);
+    });
+
+      response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log(buf);
+    });
+ });
+};
+
+
+
