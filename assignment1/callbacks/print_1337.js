9ffdfbaf898941449ba970306a496140dad636c6
Sara Han <marsa926@gmail.com>
2017-01-16 21:29:32 +0000

    added new callback directories, with other module runner files

---
+++ assignment1/callbacks/print_1337.js
@@ -0,0 +1,14 @@
+var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/1337.html'
+};
+
+function print_1337 (html) {
+
+  /* Write your code here! */
+
+}
+
+getHTML(requestOptions, print_1337);
