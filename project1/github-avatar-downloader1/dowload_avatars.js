2adc58ff5ad9f2145d8cb1b71db9074ec82b1826
Sara Han <marsa926@gmail.com>
2017-01-17 00:43:36 +0000

    made request and printed

--- project1/github-avatar-downloader1/dowload_avatars.js
+++ project1/github-avatar-downloader1/dowload_avatars.js
@@ -6,12 +6,26 @@ var GITHUB_USER = "marsa926";
 var GITHUB_TOKEN = "01dc079d65f0c0355b38bb4388e04ca227341553";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers:{
+      'User-Agent':'request'
+    }
+  };
+  request(options,function(error, response, body){
+  if (!error && response.statusCode ==200){
+
+  cb(JSON.parse(body));
+  }
+});
+
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
+
+
+
+
+getRepoContributors("jquery", "jquery", function(result) {
   console.log("Result:", result);
 });
 
